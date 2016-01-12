6584f42f01fdb4ab4be291837741bdadab7add69
Vincent van Haaff <vincent@vanhaaff.com>
2016-01-12 10:27:46 -0800

    first commit!

---
+++ data_scrape.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require 'benchmark'
+require 'csv'
+require 'pp'
+require 'smarter_csv'
+
+contacts_filepath = '../_backups/2016.01.11.utf8/Contact.csv'
+campaign_members_filepath = '../_backups/2016.01.11.utf8/CampaignMember.csv'
+
+#campaign_members = SmarterCSV.process(campaign_members_filepath)
+pp Benchmark.bm do |bm|
+	bm.report do
+    contacts = SmarterCSV.process(contacts_filepath)
+  end
+
+  #bm.report do |bm|
+    #campaign_members = SmarterCSV.process(campaign_members_filepath)
+  #end
+end
